f3861365936a52cd6c6f9e2348556480abd2c717
Junzhu Su <Cindy@Junzhus-MacBook-Pro.local>
2015-06-09 22:57:57 -0700

    Make coin yellow

--- game.js
+++ game.js
@@ -724,6 +724,8 @@ Coin = function () {
               0, -5]);
 
   this.collidesWith = ["ship"];
+  this.color='yellow';
+  this.fill=ture;
 
   this.newPosition = function () {
     this.x = Math.random() * Game.canvasWidth;
